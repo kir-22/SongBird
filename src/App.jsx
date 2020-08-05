9ccc200725990e7724e80a089362d41543a02207
Kiryl Kireyeu <kirill89kireew@gmail.com>
2020-08-05 17:49:41 +0300

    add the data birds and menu points

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Question from './components/Question/Question';
 import './index.scss';
+import Birds from './data/data.js';
 
 // const App = () => {
 //   return (
@@ -15,11 +16,67 @@ import './index.scss';
 
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.menu = [
+      {
+        id: 1,
+        name: 'Разминка',
+      },
+      {
+        id: 2,
+        name: 'Воробьиные',
+      },
+      {
+        id: 3,
+        name: 'Лесные птицы',
+      },
+      {
+        id: 4,
+        name: 'Певчие птицы',
+      },
+      {
+        id: 5,
+        name: 'Хищные птицы',
+      },
+      {
+        id: 6,
+        name: 'Морские птицы',
+      },
+    ];
+  }
+  state = {
+    score: 0,
+    level: 0,
+    birds: Birds[0],
+  };
+  componentDidMount() {
+    console.log(Birds);
+  };
+
+  next = () => {
+    this.setState({
+      level: this.state.level + 1,
+      birds: Birds[this.state.level + 1],
+    });
+  };
+
+  changeScore = value => {
+    this.setState({
+      score: value,
+    });
+  };
+
+  render() {console.log('birds', this.state.birds);
     return (
       <div className={`container-fluid app`}>
-        <Header/>
-        <Question/>
+        <Header
+          score={this.state.score}
+          level={this.state.level}
+          menu={this.menu}
+        />
+        <Question />
+        <button onClick={() => {this.next()}}>Левел UP</button>
       </div>
     );
   }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import styles from './styles/Header.module.scss';
 
-const Header = ({score = 0}) => {
+const Header = ({score = 0, menu = [], level = 0}) => {
     return (
-      <div className={`${styles.header} container`}>
-        <div>SongBird</div>
-        <div>Score: { score }</div>
+      <div className='container'>
+        <div className={styles.header}>
+          <div>SongBird</div>
+          <div>Score: { score }</div>
+        </div>
+        <ul className={styles.listContainer}>
+          {
+            menu.map((el, index) => {
+              return <li key={index} className={`${styles.listItem} ${ level === index ? styles.active : ''}`}>{el.name}</li>
+            })
+          }
+        </ul>
       </div>
     );
 }
